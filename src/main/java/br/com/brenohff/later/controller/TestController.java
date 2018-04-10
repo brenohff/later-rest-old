69177c44aa83819a66d132adfcab5bf3feeb7dc2
breno.franco <breno.franco@T11204B23074575.dinfor.uniceub>
2018-04-10 10:29:38 -0300

    Removendo JSONObject

--- src/main/java/br/com/brenohff/later/controller/TestController.java
+++ src/main/java/br/com/brenohff/later/controller/TestController.java
@@ -1,6 +1,5 @@
 package br.com.brenohff.later.controller;
 
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +8,7 @@ public class TestController {
 	
 	@RequestMapping(value = "/version", produces = "application/json")
 	public String makeTest() {
-		
-		return JSONObject.quote("v1");
+		return "v1";
 	}
 
 }
