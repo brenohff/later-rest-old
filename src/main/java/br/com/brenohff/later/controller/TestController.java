f6d3155b5574383bdfb5bdc1f251533ebd0a9b02
breno.franco <breno.franco@T11204B23074575.dinfor.uniceub>
2018-04-10 10:39:47 -0300

    Fazendo classe de teste retornar os valores corretos e em JSON.

--- src/main/java/br/com/brenohff/later/controller/TestController.java
+++ src/main/java/br/com/brenohff/later/controller/TestController.java
@@ -1,14 +1,24 @@
 package br.com.brenohff.later.controller;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-	
-	@RequestMapping(value = "/version", produces = "application/json")
-	public String makeTest() {
-		return "v1";
+
+	@RequestMapping(value = "/version", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, String> sayHello() {
+	    HashMap<String, String> map = new HashMap<>();
+	    map.put("version", "v1");
+	    map.put("date", new Date().toString());
+	    map.put("autor", "Breno Henrique");
+	    map.put("working", "true");
+	    return map;
 	}
 
 }
