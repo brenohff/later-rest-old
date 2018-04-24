e4a0dd77518dfb5a162f0241148c676e86796333
Breno Henrique Ferreira Franco <brenohff@gmail.com>
2018-04-24 00:46:51 -0300

    Alterações no controller das categorias

--- src/main/java/br/com/brenohff/later/controller/CategoryController.java
+++ src/main/java/br/com/brenohff/later/controller/CategoryController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.brenohff.later.models.LTCategory;
+import br.com.brenohff.later.models.LTEvent;
 import br.com.brenohff.later.service.CategoryService;
 
 @RestController
@@ -19,8 +21,9 @@ public class CategoryController {
 	@Autowired
 	CategoryService service;
 
-	@RequestMapping(value = "/save")
-	private ResponseEntity<Void> saveCategory() {
+	@RequestMapping(value = "/saveCategory")
+	private ResponseEntity<Void> saveCategory(@RequestBody LTCategory category) {
+		service.saveCategories(category);
 		return ResponseEntity.ok().build();
 	}
 
@@ -39,4 +42,34 @@ public class CategoryController {
 		return ResponseEntity.status(HttpStatus.OK).body(ltCategory);
 	}
 
+	/**
+		<!-- CORES CATEGORIAS -->
+	    <color name="pop">#2196F3</color>
+	    <color name="pop_700">#1976D2</color>
+	
+	    <color name="funk">#E91E63</color>
+	    <color name="funk_700">#C2185B</color>
+	
+	    <color name="house">#00BCD4</color>
+	    <color name="house_700">#0097A7</color>
+	
+	    <color name="eletronico">#9C27B0</color>
+	    <color name="eletronico_700">#7B1FA2</color>
+	
+	    <color name="rock">#F44336</color>
+	    <color name="rock_700">#D32F2F</color>
+	
+	    <color name="axe">#FFC107</color>
+	    <color name="axe_700">#FFA000</color>
+	
+	    <color name="sertanejo">#FF9800</color>
+	    <color name="sertanejo_700">#F57C00</color>
+	
+	    <color name="gospel">#CDDC39</color>
+	    <color name="gospel_700">#AFB42B</color>
+	
+	    <color name="forro">#795548</color>
+	    <color name="forro_700">#5D4037</color> 
+	 */
+	
 }
