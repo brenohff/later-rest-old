b3fafffdc96e9a116df86c6971cef862dffe539e
Breno Henrique Ferreira Franco <breno.franco@cabal.com.br>
2018-04-25 18:06:50 -0300

    Criando objeto Data

--- src/main/java/br/com/brenohff/later/controller/VersionController.java
+++ src/main/java/br/com/brenohff/later/controller/VersionController.java
@@ -1,22 +1,67 @@
 package br.com.brenohff.later.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersionController {
 
-	@RequestMapping(value = {"/", "/version"}, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, String> sayHello() {
-	    HashMap<String, String> map = new HashMap<>();
-	    map.put("version", "0.0.1-SNAPSHOT");
-	    map.put("autor", "Breno Henrique");
-	    map.put("working", "true");
-	    return map;
+	@RequestMapping(value = { "/", "/version" }, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Data> sayHello() {
+		Data data = new Data("0.0.2-SNAPSHOT", "Breno Henrique", "brenohff@gmail.com", true);
+		return ResponseEntity.status(HttpStatus.OK).body(data);
+	}
+
+}
+
+class Data {
+
+	private String version;
+	private String author;
+	private String contact;
+	private boolean working;
+
+	public Data(String version, String author, String contact, boolean working) {
+		super();
+		this.version = version;
+		this.author = author;
+		this.contact = contact;
+		this.working = working;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public boolean isWorking() {
+		return working;
+	}
+
+	public void setWorking(boolean working) {
+		this.working = working;
 	}
 
 }
