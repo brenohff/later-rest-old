96cb0fea23a7e36ad4f00014c962f33329ce4dfb
Breno Henrique Ferreira Franco <breno.franco@cabal.com.br>
2018-04-25 15:13:14 -0300

    Alterações no Application e VersionController

--- src/main/java/br/com/brenohff/later/controller/VersionController.java
+++ src/main/java/br/com/brenohff/later/controller/VersionController.java
@@ -1,6 +1,5 @@
 package br.com.brenohff.later.controller;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,8 +13,7 @@ public class VersionController {
 	@RequestMapping(value = {"/", "/version"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, String> sayHello() {
 	    HashMap<String, String> map = new HashMap<>();
-	    map.put("version", "v1");
-	    map.put("date", new Date().toString());
+	    map.put("version", "0.0.1-SNAPSHOT");
 	    map.put("autor", "Breno Henrique");
 	    map.put("working", "true");
 	    return map;
