a3b0e76bc8b765538b5fb2570923788534b3003e
Breno Henrique <brenohff@gmail.com>
2018-09-10 17:26:27 -0300

    Criando enums para estado do evento e tipo de mensagem.

--- src/main/java/br/com/brenohff/later/api/EventController.java
+++ src/main/java/br/com/brenohff/later/api/EventController.java
@@ -1,5 +1,6 @@
 package br.com.brenohff.later.api;
 
+import br.com.brenohff.later.enums.EventStatus;
 import br.com.brenohff.later.model.LTEvent;
 import br.com.brenohff.later.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,9 +45,9 @@ public class EventController {
         return service.getEventsByUser(user_id);
     }
 
-    @GetMapping(value = "/deleteEventById")
-    public void deleteEventById(@RequestParam(value = "event_id") Long event_id) {
-        service.deleteEventById(event_id);
+    @GetMapping(value = "/changeEventStatus")
+    public void changeEventStatus(@RequestParam(value = "event_status") EventStatus eventStatus) {
+        service.changeEventStatus(eventStatus);
     }
 
 }
--- src/main/java/br/com/brenohff/later/repository/EventRepository.java
+++ src/main/java/br/com/brenohff/later/repository/EventRepository.java
@@ -1,5 +1,6 @@
 package br.com.brenohff.later.repository;
 
+import br.com.brenohff.later.enums.EventStatus;
 import br.com.brenohff.later.model.LTEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,7 +16,7 @@ public interface EventRepository extends JpaRepository<LTEvent, Long> {
     @Query("SELECT e FROM LTEvent e WHERE e.isPrivate = false")
     List<LTEvent> getPublic();
 
-    @Query("DELETE FROM LTEvent e WHERE e.id = :event_id")
-    void deleteById(@Param("event_id") Long event_id);
+    @Query("UPDATE LTEvent e SET e.status = :eventStatus")
+    void changeEventStatus(@Param("eventStatus") EventStatus eventStatus);
 
 }
