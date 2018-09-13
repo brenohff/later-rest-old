f95b6a4c7723d6a875299eac60261def064c4a7e
Breno Henrique <brenohff@gmail.com>
2018-09-12 23:23:44 -0300

    Ajustando tamanho da coluna description; Corrigindo erro no update Versão 1.0.3

--- src/main/java/br/com/brenohff/later/repository/EventRepository.java
+++ src/main/java/br/com/brenohff/later/repository/EventRepository.java
@@ -3,9 +3,11 @@ package br.com.brenohff.later.repository;
 import br.com.brenohff.later.enums.EventStatus;
 import br.com.brenohff.later.model.LTEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<LTEvent, Long> {
@@ -16,6 +18,8 @@ public interface EventRepository extends JpaRepository<LTEvent, Long> {
     @Query("SELECT e FROM LTEvent e WHERE e.isPrivate = false")
     List<LTEvent> getPublic();
 
+    @Transactional
+    @Modifying
     @Query("UPDATE LTEvent e SET e.status = :eventStatus")
     void changeEventStatus(@Param("eventStatus") EventStatus eventStatus);
 
